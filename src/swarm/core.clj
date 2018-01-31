ac987aeb721827aa5118a913580b2c9836f5be25
Dimitri Lozeve <dimitri.lozeve@gmail.com>
2018-01-31 11:28:50 +0000

    Update docstrings

--- src/swarm/core.clj
+++ src/swarm/core.clj
@@ -3,16 +3,16 @@
             [quil.middleware :as m]))
 
 (defn generate-boid []
-  ;; Generate a random boid
+  "Generate a random boid"
   {:x (/ (- (rand 2) 1) 10)
    :y (/ (- (rand 2) 1) 10)
    :dx 0
    :dy 0})
 
 (defn generate-boids [n]
-  ;; Generate n random boids
+  "Generate n random boids"
   {:boids (vec (repeatedly n generate-boid))
-   :goal {:x 0 :y 0}})
+   :goal  {:x 0 :y 0}})
 
 (defn setup [n]
   (q/frame-rate 30)
@@ -20,31 +20,30 @@
   (generate-boids n))
 
 (defn distance [b1 b2]
-  ;; Get the distance between two boids
+  "Get the distance between two boids"
   (q/sqrt (+ (q/sq (- (:x b1) (:x b2)))
              (q/sq (- (:y b1) (:y b2))))))
 
 (defn get-neighbours [threshold b bs]
-  (vec (filter #(< (distance b %) threshold) bs))
-  )
+  "Get boids closer to a certain threshold"
+  (vec (filter #(< (distance b %) threshold) bs)))
 
 (defn center-of-mass [boids]
-  ;; Compute the center of mass of a set of boids
+  "Compute the center of mass of a set of boids"
   (if (zero? (count boids))
     {:x 0 :y 0}
     {:x (float (/ (apply + (for [b boids] (:x b))) (count boids)))
      :y (float (/ (apply + (for [b boids] (:y b))) (count boids)))}))
 
 (defn cohesion [b neighbours]
-  ;; Cohesion rule: boids try to move towards the center of mass of
-  ;; their neighbours
+  "Cohesion rule: boids try to move towards the center of mass of
+  their neighbours"
   (let [c (center-of-mass neighbours)]
     {:dx (/ (- (:x c) (:x b)) 100)
      :dy (/ (- (:y c) (:y b)) 100)}))
 
 (defn separation [b neighbours]
-  ;; Separation rule: boids move away from neighbours that are too
-  ;; close
+  "Separation rule: boids move away from neighbours that are too close"
   (let [too-close (filter #(< (distance b %) 0.03) neighbours)
         close {:x (apply + (for [c too-close] (- (:x b) (:x c))))
                :y (apply + (for [c too-close] (- (:y b) (:y c))))}]
@@ -52,18 +51,19 @@
      :dy (/ (:y close) 2)}))
 
 (defn alignment [b neighbours]
-  ;; Alignment rule: boids try to adopt the same velocity as their
-  ;; neighbours
+  "Alignment rule: boids try to adopt the same velocity as their neighbours"
   (if (zero? (count neighbours))
     {:dx 0 :dy 0}
     {:dx (/ (apply + (for [b neighbours] (:dx b))) (* 10 (count neighbours)))
      :dy (/ (apply + (for [b neighbours] (:dy b))) (* 10 (count neighbours)))}))
 
 (defn goal-seeking [boid goal]
+  "Move towards a goal point"
   {:dx (/ (- (:x goal) (:x boid)) 100)
    :dy (/ (- (:y goal) (:y boid)) 100)})
 
 (defn limit-velocity [dx dy]
+  "Limit the maximum velocity of a boid"
   (let [m (q/sqrt (+ (* dx dx) (* dy dy)))
         max-v 0.035]
     (if (> m max-v)
@@ -71,6 +71,8 @@
       [dx dy])))
 
 (defn bound-position [boid]
+  "Limit the position of a boid: if it moves out of the zone, get it
+  to move back"
   (let [dx (cond
              (> (:x boid) 1) -0.025
              (< (:x boid) -1) 0.025
@@ -82,7 +84,7 @@
     {:dx dx :dy dy}))
 
 (defn update-boid [b bs goal]
-  ;; Update a boid's position
+  "Update a boid's position"
   (let [neighbours (get-neighbours 0.5 b bs)
         dist-update [(cohesion b neighbours)
                      (separation b neighbours)
@@ -98,12 +100,13 @@
      :dx dx-limited :dy dy-limited}))
 
 (defn update-state [state]
-  ;; Update the positions of all boids
+  "Update the positions of all boids"
   {:boids (vec (for [b (:boids state)]
                  (update-boid b (:boids state) (:goal state))))
    :goal (:goal state)})
 
 (defn mouse-moved [state event]
+  "When the mouse is moved, set the goal to its position"
   (let [x (- (* (/ (:x event) (q/width)) 2) 1)
         y (- (* (/ (:y event) (q/height)) 2) 1)]
     (assoc-in state [:goal]
@@ -117,7 +120,7 @@
     (q/ellipse (* (q/width) (:x b)) (* (q/height) (:y b)) 5 5)))
 
 (q/defsketch swarm
-  :title "The Swarm"
+  :title "swarm"
   :size [800 800]
                                         ; setup function called only once, during sketch initialization.
   :setup #(setup 200)
